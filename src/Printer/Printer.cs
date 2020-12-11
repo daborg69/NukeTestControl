a09f2768183b1a558c8597e956ab117e10db41da
Scott Herrmann - GitHub Hiddent Email <1109038+daborg69@users.noreply.github.com>
2020-12-11 08:45:49 -0500

    Printing Fix

--- src/Printer/Printer.cs
+++ src/Printer/Printer.cs
@@ -18,6 +18,7 @@ namespace Daborg69.Utility
 			Console.WriteLine("Ink Replaced!");
 			Console.WriteLine("Continuing to Print...");
 			Console.WriteLine("Still Printing...");
+			Console.WriteLine("Still Printing...");
 		}
 	}
 }
